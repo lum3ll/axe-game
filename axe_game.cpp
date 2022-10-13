17a57c0bfc1d10ba09191be582cc1ef2efbfb635
Luca Mellano <luca.mellano@arsdigitalia.it>
2022-10-13 21:34:08 +0100

    Draw rectangle (axe) Move axe up and down and bounce on windows vertical limits.

--- axe_game.cpp
+++ axe_game.cpp
@@ -10,8 +10,14 @@ int main()
     InitWindow(width, height, "Luca's Game Window");
 
     // Circle coordinates
-    int circle_x{600};
+    int circle_x{400};
     int circle_y{400};
+
+    // Axe coordinates
+    int axe_x{575};
+    int axe_y{0};
+
+    int direction{10};
 
     // Set target FPS to 60 and handle movements.
     SetTargetFPS(60);
@@ -20,13 +26,25 @@ int main()
     while(WindowShouldClose() != true) 
     {
         BeginDrawing();
+
         // Clear background and change color
         ClearBackground(WHITE);             
 
-        // Game logic begins
+        /*
+        Game logic begins
+        */
 
         // Draw a circle in the middle of the canvas
         DrawCircle(circle_x, circle_y, 25, BLUE); 
+
+        // Draw a rectangle in specific position
+        DrawRectangle(axe_x, axe_y, 50, 50, RED);
+
+        axe_y += direction;
+        if(axe_y > height || axe_y < 0) 
+        {
+            direction = -direction;
+        }
 
         // Check if key D is pressed to move circle to the right
         if (IsKeyDown(KEY_D) && circle_x < width)
@@ -40,7 +58,10 @@ int main()
             circle_x -= 10;
         }
         
-        // Game login ends
+        /*
+        Game logic ends
+        */
+
         EndDrawing();
     }
 }
